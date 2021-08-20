84072d21a6b7361dfc35e1c83a5d4e9b5770c443
daniloiiveroy <daniloiiveroy@gmail.com>
2021-08-20 17:08:41 +0800

    Add key-value training

---
+++ key-value/app.py
@@ -0,0 +1,51 @@
+student = {
+    "Name" : "Dan",
+    "Age" : 25,
+    "Course" : ["Math" , "CompSci"]
+}
+
+print(student) #sample model
+print(student["Course"]) #get specific key
+print(student.get("Name")) #get method
+print(student.get("Address")) #get method with error
+print(student.get("Address","404 Not Found")) #get method with error and return message
+
+#insert value with key
+student["Address"] = "Bulacan"
+print(student)
+
+#update value
+update_student = {
+    "Name" : "Dan",
+    "Age" : 27,
+    "Course" : ["Math" , "CompSci"]
+}
+
+student.update(update_student)
+print(student)
+
+#delete key
+del student["Address"]
+print(student)
+
+#pop function
+age = student.pop("Age")
+print(student)
+print(age)
+
+#lenght function
+print(len(student))
+
+#keys, values and items
+print(student.keys())
+print(student.values())
+print(student.items())
+
+#for loop
+for key, value in student.items():
+    print(key, value)
+
+'''
+clear
+& "D:/Program Files/Python39/python.exe" d:/Solutions/PythonTraining/key-value/app.py
+'''
